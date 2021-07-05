8c33f7d72d4430d222648b44e425e157f2faa773
Archie <alimkul.shynggys@gmail.com>
2021-07-05 21:44:11 +0600

    MVVM2

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -35,9 +35,6 @@ class MainActivity : AppCompatActivity() {
 
                 sharecount.text = checkCount(post.sharesCount)
 
-
-
-
                 likes?.setOnClickListener {
                     post.likedByMe = !post.likedByMe
                     likes.setImageResource(
